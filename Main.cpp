6c47f0e496b4d896d1d88b3b507dfce57acadde7
daniil2231 <danit22231@gmail.com>
2024-10-14 00:51:38 +0200

    chore: document code in main

--- Main.cpp
+++ Main.cpp
@@ -16,6 +16,7 @@ int main()
 
     OrderBook order_book;
 
+    // Fill up the order book with a few dummy orders:
     for (Order o : initial_orders) {
         order_book.placeLimitOrder(o.getDirection(), o.getPrice(), o.getSize());
     }
@@ -23,6 +24,7 @@ int main()
     OrderQueue ob_buy_orders = order_book.getAllBuyOrders();
     OrderQueue ob_sell_orders = order_book.getAllSellOrders();
 
+    // Print out all the orders in the order book:
     while (!ob_buy_orders.empty()) {
         Order topOrder = ob_buy_orders.top();
         std::cout << topOrder << std::endl;
@@ -37,8 +39,10 @@ int main()
     }
     std::cout << std::endl;
 
+    // Dummy cmp to test placing market orders:
     double current_market_price = 1000.0;
 
+    // Placing a market buy order and checking if the sell order closest to the market one's price is no longer a limit in the order book.
     order_book.placeMarketOrder("buy", current_market_price, 0.05);
     std::cout << order_book.getAllSellOrders().top();
 }
